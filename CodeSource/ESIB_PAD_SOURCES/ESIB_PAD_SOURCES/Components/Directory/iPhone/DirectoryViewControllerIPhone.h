a26aeb5ccd2009cafc1063bd76b66e309d2bbff8
Medawar Elias <elias.medawar@gmail.com>
2011-07-20 13:25:26 +0300

    Adding the directory component

---
+++ CodeSource/ESIB_PAD_SOURCES/ESIB_PAD_SOURCES/Components/Directory/iPhone/DirectoryViewControllerIPhone.h
@@ -0,0 +1,16 @@
+//
+//  DirectoryViewControllerIPhone.h
+//  ESIB_PAD_SOURCES
+//
+//  Created by Elias Medawar on 15.07.11.
+//  Copyright 2011 HEFR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface DirectoryViewControllerIPhone : UIViewController {
+    
+}
+
+@end
